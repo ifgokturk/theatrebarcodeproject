47541c4037456f3bb4074e3be65839ff856c8bd2
İbrahim Fatih GÖKTÜRK <102929252+ifgokturk@users.noreply.github.com>
2022-08-18 13:27:04 +0300

    Update Sabitler.dart

--- lib/constants/Sabitler.dart
+++ lib/constants/Sabitler.dart
@@ -8,12 +8,12 @@ class sabitler {
   static const Color clr_orange = Color(0xFFFF682D);
   static const Color clr_light_grey = Color(0xAAD3D3D3);
 
-  //static const String url = "http://172.22.20.255/mobilservice";
-  static const String url = "api.kocaeli.bel.tr";
-  static const String urlGetSeans = "/TiyatroApi/api/GetSeansList";
-  static const String urlBiletNo = "/TiyatroApi/api/GetBiletDetay?BiletNo=";
+  //static const String url = "";
+  static const String url = "";
+  static const String urlGetSeans = "/api/GetSeansList";
+  static const String urlBiletNo = "/api/GetBiletDetay?BiletNo=";
   // static const String loginApi = url + "/api/CheckUser";
-  static const String urlSalonGiris = "/TiyatroApi/api/BiletSalonGiris";
+  static const String urlSalonGiris = "/api/";
 
   static String userData = "USER_DATA";
   static String isOnBoard = "IS_ONBOARD";
@@ -24,11 +24,7 @@ class sabitler {
       "^([0-9a-zA-Z]([-.+\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})\$";
   static const String PATTERN_GSM = "([(+]*[0-9]+[()+. -]*)";
 
-  //URL For list of photos
-  // static const String accessKey =
-  //     "f9a49fde-8b0a-4a1f-bd13-0a49749226e1";
+  
 
-  // static const String accessToken = "D9D39CBB-7EE9-46E8-80A1-3A64F589AA29";
-
-  static const String authorization = "b44ef1e1-e03e-43cc-9391-29906ec77977";
+  static const String authorization = "---";
 }
